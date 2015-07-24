0fa3501dbf2b49b8e34f580ea9d546f3f9ac83ee
caiges <caigesn@gmail.com>
2015-07-24 03:36:43 +0000

    Initial commit

---
+++ content/content/about.md
@@ -0,0 +1,8 @@
++++
+date = "2015-07-24T02:35:50Z"
+draft = true
+title = "about"
+
++++
+
+Blars eats tacos
---
+++ serve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hugo server -b http://nio.replrapl.com/ --bind=0.0.0.0 --theme=simple-hugo --buildDrafts -w
